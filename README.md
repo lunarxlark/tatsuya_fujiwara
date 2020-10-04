3bc1bbb04477e91f22105ada3a1bd6a77d775017
lunarxlark <lunarxlark@gmail.com>
2020-10-04 22:17:10 +0900

    fist commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Fujiwara tatsuya Tool
+
+```bash
+$ fujiwara shout どうしてなんだよおおぉ！！！
+ど゜う゛゛し゛て゛な゛ん゛だ゛よ゛お゛お゛ぉ゛！゛！゛！゛゛
+```
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	if err := newApp().Run(os.Args); err != nil {
+	}
+}
+
+var commands = []*cli.Command{
+	{Name: "shout", Action: doShout},
+}
+
+func newApp() *cli.App {
+	app := cli.NewApp()
+	app.Name = "fujiwara"
+	app.Usage = "respect tatsuya fujiwara"
+	app.Version = "0.0.1"
+	app.Authors = []*cli.Author{
+		{Name: "lunarxlark", Email: "lunarxlark@gmail.com"},
+	}
+	app.Commands = commands
+
+	return app
+}
+
+func doShout(ctx *cli.Context) error {
+	args := ctx.Args().Slice()
+	msg := strings.Join(args, " ")
+	for _, c := range msg {
+		fmt.Printf("%v゛", string([]rune{c}))
+	}
+
+	return nil
+}
